81e2b4b2244414c3546d16a75ba52d1bd8ff9636
James Turk <consulting@jamesturk.net>
2022-08-07 14:16:22 -0500

    add tests for Fresno Planning Commission

---
+++ tests/test_fresno_planning_commission.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from os.path import dirname, join
+
+import pytest
+from city_scrapers_core.constants import NOT_CLASSIFIED
+from city_scrapers_core.utils import file_response
+from freezegun import freeze_time
+
+from city_scrapers.spiders.fresno_planning_commission import FresnoPlanningCommissionSpider
+
+test_response = file_response(
+    join(dirname(__file__), "files", "fresno_planning_commission.html"),
+    url="https://www.co.fresno.ca.us/departments/public-works-planning/divisions-of-public-works-and-planning/development-services-division/planning-and-land-use/planning-commission/plann/-toggle-allupcoming",
+)
+spider = FresnoPlanningCommissionSpider()
+
+freezer = freeze_time("2022-08-07")
+freezer.start()
+
+parsed_items = [item for item in spider.parse(test_response)]
+
+freezer.stop()
+
+
+def test_title():
+    assert parsed_items[0]["title"] == "Planning Commission Meeting"
+
+
+def test_start():
+    assert parsed_items[0]["start"] == datetime(2022, 8, 11, 8, 45)
+
+
+def test_end():
+    assert parsed_items[0]["end"] == datetime(2022, 8, 11, 12, 59)
