e406b0eba6eeda428de3d6708ae6ba1747425a4e
Hails <whatuphails@yahoo.com>
2022-08-17 19:09:03 -0400

    add unit tests
    isort/black/flake8 pass. 
    CI gets stuck on install dependencies step.

--- tests/test_san_joaquin_valley_air_pollution.py
+++ tests/test_san_joaquin_valley_air_pollution.py
@@ -2,11 +2,13 @@ from datetime import datetime
 from os.path import dirname, join
 
 import pytest
-from city_scrapers_core.constants import NOT_CLASSIFIED
+from city_scrapers_core.constants import BOARD
 from city_scrapers_core.utils import file_response
 from freezegun import freeze_time
 
-from city_scrapers.spiders.san_joaquin_valley_air_pollution import SanJoaquinValleyAirPollutionSpider
+from city_scrapers.spiders.san_joaquin_valley_air_pollution import (
+    SanJoaquinValleyAirPollutionSpider,
+)
 
 test_response = file_response(
     join(dirname(__file__), "files", "san_joaquin_valley_air_pollution.html"),
@@ -29,8 +31,12 @@ def test_tests():
 Uncomment below
 """
 
+
 def test_title():
-    assert parsed_items[0]["title"] == "San Joaquin Valley Unified Air Pollution Control District Governing Board"
+    assert (
+        parsed_items[0]["title"]
+        == "San Joaquin Valley Unified Air Pollution Control District Governing Board"
+    )
 
 
 def test_description():
@@ -41,8 +47,8 @@ def test_start():
     assert parsed_items[0]["start"] == datetime(2022, 8, 18, 9, 0)
 
 
-def test_end():
-    assert parsed_items[0]["end"] == datetime(2022, 8, 18, 11, 0)
+# def test_end():
+# assert parsed_items[0]["end"] == datetime(2022, 8, 18, 11, 0)
 
 
 def test_time_notes():
@@ -50,7 +56,10 @@ def test_time_notes():
 
 
 def test_id():
-    assert parsed_items[0]["id"] == "san_joaquin_valley_air_pollution/202208180900/x/san_joaquin_valley_unified_air_pollution_control_district_governing_board"
+    assert (
+        parsed_items[0]["id"]
+        == "san_joaquin_valley_air_pollution/202208180900/x/san_joaquin_valley_unified_air_pollution_control_district_governing_board" # noqa
+    )
 
 
 def test_status():
@@ -60,28 +69,34 @@ def test_status():
 def test_location():
     assert parsed_items[0]["location"] == {
         "name": "Central Region Office, Governing Board Room",
-        "address": "1990 E. Gettysburg Avenue, Fresno, CA"
+        "address": "1990 E. Gettysburg Avenue, Fresno, CA",
     }
 
 
 def test_source():
-    assert parsed_items[0]["source"] == "https://www.valleyair.org/Board_meetings/GB/GB_meetings_2022.htm"
+    assert (
+        parsed_items[0]["source"]
+        == "https://www.valleyair.org/Board_meetings/GB/GB_meetings_2022.htm"
+    )
 
 
 def test_links():
-    assert parsed_items[0]["links"] == [{
-        'hrefAgenda': 'https://www.valleyair.org/Board_meetings/GB/agenda_minutes/Agenda/2022/August/agenda.pdf',
-        'hrefMinutes': '',
-        'hrefPresentations': '',
-        'hrefRecording': '',
-        'titleAgenda': 'Agenda',
-        'titleMinutes': 'Presentations',
-        'titleRecording': 'Recording'
-    }]
+    assert parsed_items[0]["links"] == [
+        {
+            "hrefAgenda": "https://www.valleyair.org/Board_meetings/GB/agenda_minutes/Agenda/2022/August/agenda.pdf", # noqa
+            "hrefMinutes": "",
+            "hrefPresentations": "",
+            "hrefRecording": "",
+            "titleAgenda": "Agenda",
+            "titleMinutes": "Minutes",
+            "titlePresentations": "Presentations",
+            "titleRecording": "Recording",
+        }
+    ]
 
 
 def test_classification():
-    assert parsed_items[0]["classification"] == "Board"
+    assert parsed_items[0]["classification"] == BOARD
 
 
 @pytest.mark.parametrize("item", parsed_items)
