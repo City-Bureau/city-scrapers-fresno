b141b28eb346246c0730331bf1006ece434c33fe
Hails <whatuphails@yahoo.com>
2022-08-29 20:37:43 -0400

    add spider

---
+++ city_scrapers/spiders/fre_visalia_city_council.py
@@ -0,0 +1,67 @@
+from city_scrapers_core.constants import CITY_COUNCIL
+from city_scrapers_core.items import Meeting
+from city_scrapers_core.spiders import LegistarSpider
+
+
+class FreVisaliaCityCouncilSpider(LegistarSpider):
+    name = "fre_visalia_city_council"
+    agency = "Visalia City Council"
+    timezone = "America/Chicago"
+    start_urls = ["https://visalia.legistar.com/Calendar.aspx"]
+    # Add the titles of any links not included in the scraped results
+    link_types = []
+
+    def parse_legistar(self, events):
+        """
+        `parse_legistar` should always `yield` Meeting items.
+
+        Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
+        needs.
+        """
+        for event in events:
+            title = event["Name"]
+            if "Closed" not in title:
+                meeting = Meeting(
+                    title=event["Name"],
+                    description=self._parse_description(event),
+                    classification=self._parse_classification(event),
+                    start=self.legistar_start(event),
+                    end=self._parse_end(event),
+                    all_day=self._parse_all_day(event),
+                    time_notes=self._parse_time_notes(event),
+                    location=self._parse_location(event),
+                    links=self.legistar_links(event),
+                    source=self.legistar_source(event),
+                )
+
+                meeting["status"] = self._get_status(meeting)
+                meeting["id"] = self._get_id(meeting)
+
+                yield meeting
+
+    def _parse_description(self, item):
+        """Parse or generate meeting description."""
+        return ""
+
+    def _parse_classification(self, item):
+        """Parse or generate classification from allowed options."""
+        return CITY_COUNCIL
+
+    def _parse_end(self, item):
+        """Parse end datetime as a naive datetime object. Added by pipeline if None"""
+        return None
+
+    def _parse_time_notes(self, item):
+        """Parse any additional notes on the timing of the meeting"""
+        return ""
+
+    def _parse_all_day(self, item):
+        """Parse or generate all-day status. Defaults to False."""
+        return False
+
+    def _parse_location(self, item):
+        """Parse or generate location."""
+        return {
+            "address": "707 W. Acequia Ave. Visalia, CA 93292",
+            "name": item.get("Meeting Location", ""),
+        }
