20532b3816e34f3369aa8ab4d082f29e7ab26c0d
Hails <whatuphails@yahoo.com>
2023-01-11 14:51:17 -0500

    Update test_avenal_city_council.py

--- tests/test_avenal_city_council.py
+++ tests/test_avenal_city_council.py
@@ -6,7 +6,7 @@ from city_scrapers_core.constants import CITY_COUNCIL
 from city_scrapers_core.utils import file_response
 from freezegun import freeze_time
 
-from city_scrapers.spiders.avenal_city_council import AvenalCityCouncilSpider
+from city_scrapers.spiders.fre_avenal_city_council import AvenalCityCouncilSpider
 
 test_response = file_response(
     join(dirname(__file__), "files", "avenal_city_council.html"),
